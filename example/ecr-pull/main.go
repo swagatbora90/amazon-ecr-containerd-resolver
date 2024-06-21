3a006d9b68607b54eac4da476137cd8730b9cc69
EC2 Default User <ec2-user@ip-172-31-21-251.us-west-2.compute.internal>
2024-06-21 19:27:46 +0000

    update to use docker resolver

--- example/ecr-pull/main.go
+++ example/ecr-pull/main.go
@@ -82,19 +82,31 @@ func main() {
 		}
 		return nil, nil
 	})
+	
+	//resolver, err := ecr.NewResolver(ecr.WithLayerDownloadParallelism(parallelism))
+	//if err != nil {
+	//	log.G(ctx).WithError(err).Fatal("Failed to create resolver")
+	//}
 
-	resolver, err := ecr.NewResolver(ecr.WithLayerDownloadParallelism(parallelism))
-	if err != nil {
-		log.G(ctx).WithError(err).Fatal("Failed to create resolver")
-	}
+	options := docker.ResolverOptions{}
+	username := "AWS"
+	secret := "eyJwYXlsb2FkIjoiZzBvZVp4YTY3bGc0SVpyRWdrd0xFQWFpNGNac2FGT0ExSitSZ0ZwWmtvKzJteW1wNm9rdjkyOGNaWUtRbnp3cGl5c1RwR2FnOWt4M3lCdUpDUlBmZjdtVEVWenZad1orSjRRZkw4b0JSZ1NQM0hRTCs3TCs5UlpsWGVqRjNkNyt6Z1FmOU1TOVlHQ0JrWTlIQlBYcVI1ankyZk9jeWgxeTREam9XQ1U2OWozY0NIMUo2QVR5UzZzYllwbzE0RjRFU1BsVkFudzN0Z1ZCMVExUUlhRmFFT2t5R3NvWitBdVlNbVNSNDV3TEV0ekJqK2NtcUlLcitmQ3ZqTFJjZzRiSVZsUFhuWGlWQmoxQnBoN0JzbnRhbVh1WGMzcVdGenZhQnlvSFlKV3o5ZjZ1ZnYxL0xYWGppTCtVY2g1Qm1ZTmNFTjB3U1hDL203elZMbG85RmJBNXNDZ2xaM3llbmtYOWkyeEJRd0I2ZFVIaWUvemIwbTd5SXF5WHBCYzVxSEw1MUdWRk5LeUc0WU12R1I2Y2tVSzlWRDUrUXNPZFdpb25VZ2ROakhwb1h5cithRGw1NDdON3hXaFZERTBtT3RaUDFXQ1RDVHFZempzUkNxUUNTUklSbGE0cGhyRlgzeDZITWlabkFCL014cmRWU2toQ3dVTWYwcjlZblR5cVZGakpiRVBqTklVdUVzV2UzM0RkbCtRZXZhNUtrZWxVcUJEYk9XU25OMEg4ZTFzTFBESVBrZWwxZEpnRmNXbWhsYVJ4aHJWaDdoOWoycmFaZjdXbWRZbVRKWThqaEJtSEhxZURIRVBNbFlDOUJwaWJRY0F3eEh1S1p5TnFTKzhqZjZsNmR3WFdZWFRxengvSzlJSWlzNEU4c3NMdDFxOWwrVEpwMDJZYnYvNjJDamoyVlZtVFVIdGllOFoyOEZUVjhuQW1KbFVzOWgrY1dKellGQjRwQWQ2TnlDNmxjU0FQdGdoMzVIRFM1OVlDc3FWZVZNRkZoNnBBZm1INHhNWVA4clIzOWZTbjBGaXFCMjFIeEF0bGwrenIyZTFkY3YzRnptSEQzekJ3VVIzU1IvdUhUR1VYK2ZUaGJMdlJzSjVFVno5Rk8xNjFJamw1WW1DSGE3S2E1aTBwODZHV2Z0RUdZcnA1ZHkzcWJZK3liSHhkRFk0bCs0clkzcTJOWkdyL0NRTDQ3Qmw4Y2ZsK01qR2tpME1Mdm5MMEwwY25sVEs0eFZzMHIvRThQOTZGczllVHR6MFB0U1JXdUFiQk9WUVZOZXQ4dUJ3S29yU1cybWV1RzRIaERCNXhTMkkxMU93NjQ2N2tHTXBUbzhhVWdCK1g2allwZWE2OWpqa3g5MzBBeFdodVhSY24wUUNGYmFJR0FlaW00VW9WQVJZVm1UM09saytCSXJpdFR6cWlVaGZ0clk2MExhVGNqQUMvOXNncjY1b1EvTkNsSmU5ZE8ybTdhdWkwVUMwOXEwdjUxRytFMWZiOTJ6MU1CbGtEK1Z3cHd4bEZuMWJqd1BHSDE3cWNCYW5mU0NqUWpvUXpIY3Q5TEQwYksrdS9hb2xiUUlrL1Zza09PQndqQUF6Z1V6cmNwWDdhWktnUStwTkoyQmNYaHdlM1hIbWtLYWVnRDFkcjc1V3k1cEVadFhCbHJDaWl4V3RMK0xEKzVHVFoyRHVrNmpaM1ZDN2hSTUsrRXpqQ1FvWWYrZklxdGYyRG5Jc3d4K3lKb2FFN0RIcFdGS1lWbzBZK2xRMTRRTzB2RW1yWDQxUWpRSjZTSUk0cVF6eitXMEhNNHZ2bFh6Ui9FVXh0UlFIYmIxK204VGFBM09RN2pBK1NpMUJmTUhLSlBCQmVSUVAxUDJQSkJOZWFQYmdYb3RWckI4bXdGL2dYYjEvUnlncjNjWU4wMm5mNnBueXNnMHJJbHcrMURvdVpjTmJtQmxESWF0blE2YmFWNEo3YnpXekEzMFpKNWw3MncyUGNjd1E2Szg1SFpGT0RBRld1MCtGNVZWNThFUHhoWWlhYnc0ZGpzcWVmTWkzM0M4NEhsQUpaQyswSjM1U2ZzSk12UlRYR3ZzMHZoS3R4WWZCRWlwaXRRQjdRSHY2d2h4a040d2xoRDM4cHUveFM4cEdnVDM4clBGc3ZwRnlDUzF1UXcrRTZXVGRnbC9uQytKcExQTDhzNHpkWmcxdnhpbHg0czdUcXhUZ3F6clpOUWovQTB4Uk9YVDJEMmlDcFh0TEh1VEttQ04yUUFMS1lWUFNhWGkzeVg4ZUoyb0JZVGFCWnpKSTRZYk9MNmVyR0hSRFpneUlMbzB3RFB3Z2wyY1FKVGJRYm9ERHV6UVFyWFVTWkxIS3VqRlAyYm9YZElNZHgzaERyTkN5encvMWpQNGlmWit0bW5HMDNWSHNRN0o3U3RpdVJJaHhYREUrdCtKTnBQTjF2eWZTYUg0cEtQS2tnb3AvSFBhV1NIRFJHQXZxMEo2UW1OYzBlRFhFN1IvT052eE96Q2ZadjdidnE4b0ZUNHkvT0QvZXdLRUVWVjFpMUpWYzRuVGtGSnU5N0poM0REWkNVOGI5Y0RUcmJyL3dreHJQc3BnPT0iLCJkYXRha2V5IjoiQVFFQkFIajZsYzRYSUp3LzdsbjBIYzAwRE1lazZHRXhIQ2JZNFJJcFRNQ0k1OEluVXdBQUFINHdmQVlKS29aSWh2Y05BUWNHb0c4d2JRSUJBREJvQmdrcWhraUc5dzBCQndFd0hnWUpZSVpJQVdVREJBRXVNQkVFRE56YmU2N1dBVHVtakVuUi9RSUJFSUE3U3BFaWUzcUptS01DL284VHdHUTdBK0NvREJmYmlZL0RIZUhTY0IvUThLTExhVER1V05pTDUwMzRBa2oyaEpSRThURi9vQXVCZnNneDd0Zz0iLCJ2ZXJzaW9uIjoiMiIsInR5cGUiOiJEQVRBX0tFWSIsImV4cGlyYXRpb24iOjE3MTkwNDExNjF9"
+	hostOptions := config.HostOptions{}
+	hostOptions.Credentials = func(host string) (string, string, error) {
+                 //If host doesn't match...
+                // Only one host
+              return username, secret, nil
+        }
+	options.Hosts = config.ConfigureHosts(ctx, hostOptions)
+	resolver := docker.NewResolver(options)
 
 	log.G(ctx).WithField("ref", ref).Info("Pulling from Amazon ECR")
 	img, err := client.Pull(ctx, ref,
 		containerd.WithResolver(resolver),
 		containerd.WithImageHandler(h),
-		containerd.WithSchema1Conversion)
-		//	containerd.WithMaxConcurrentDownloads(3),
-		//containerd.WithPullUnpack)
+		containerd.WithSchema1Conversion,
+		containerd.WithMaxConcurrentDownloads(3),
+		containerd.WithPullUnpack)
 	stopProgress()
 	if err != nil {
 		log.G(ctx).WithError(err).WithField("ref", ref).Fatal("Failed to pull")
